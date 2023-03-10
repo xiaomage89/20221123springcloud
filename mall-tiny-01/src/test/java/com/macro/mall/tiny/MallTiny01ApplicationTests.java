05b0cf3dc8523d1f82213e876d3a46e748ed5f6f
majunjie_yx@163.com <45347041+xiaomage89@users.noreply.github.com>
2023-03-10 16:06:59 +0800

    test

--- mall-tiny-01/src/test/java/com/macro/mall/tiny/MallTiny01ApplicationTests.java
+++ mall-tiny-01/src/test/java/com/macro/mall/tiny/MallTiny01ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.stock;
+package com.macro.mall.tiny;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StockService1ApplicationTests {
+class MallTiny01ApplicationTests {
 
     @Test
     void contextLoads() {
